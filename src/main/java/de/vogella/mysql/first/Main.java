94d4b0a381fcabc480f7b92eb0157b34df3520e6
jelenamijailovic <jmijailovic@gmail.com>
2018-03-27 14:45:48 +0200

    commit pull1 vogella

---
+++ src/main/java/de/vogella/mysql/first/Main.java
@@ -0,0 +1,11 @@
+package de.vogella.mysql.first;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		MySQLAccess dao= new MySQLAccess();
+		dao.readDataBase();
+
+	}
+
+}
